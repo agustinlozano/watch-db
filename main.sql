e64f5ce4c0661fb42b276e2a19b8e8e6f5573ae9
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-05-15 14:09:39 -0300

    Complete the 'case' table with other tables

--- main.sql
+++ main.sql
@@ -18,3 +18,63 @@ CREATE TABLE `Case` (
   `id_shape` INTEGER NULL DEFAULT NULL,
   PRIMARY KEY (`id_case`)
 );
+
+-- ---
+-- Table 'Crystal'
+-- 
+-- ---
+DROP TABLE IF EXISTS `Crystal`;
+
+CREATE TABLE `Crystal` (
+  `id_crystal` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `crystal_type` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_crystal`)
+);
+
+-- ---
+-- Table 'Material'
+-- 
+-- ---
+DROP TABLE IF EXISTS `Material`;
+
+CREATE TABLE `Material` (
+  `id_material` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `material_type` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_material`)
+);
+
+-- ---
+-- Table 'Color'
+-- 
+-- ---
+DROP TABLE IF EXISTS `Color`;
+
+CREATE TABLE `Color` (
+  `id_color` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `shape_type` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_color`)
+);
+
+-- ---
+-- Table 'Pusher'
+-- 
+-- ---
+DROP TABLE IF EXISTS `Pusher`;
+
+CREATE TABLE `Pusher` (
+  `id_pusher` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `type_pusher` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_pusher`)
+);
+
+-- ---
+-- Table 'Shape'
+-- 
+-- ---
+DROP TABLE IF EXISTS `Shape`;
+
+CREATE TABLE `Shape` (
+  `id_shape` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `shape_type` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_shape`)
+);
