1ccceaf45b849aecc0d293f6a5f4f59b0242b8b0
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-05-15 14:51:27 -0300

    Add the 'dial' table with other tables

--- main.sql
+++ main.sql
@@ -40,7 +40,7 @@ CREATE TABLE `Crystal` (
 -- 
 -- ---
 
-DROP TABLE IF EXISTS `Material`;
+DROP TABLE IF EXISTS `Material`;g
 		
 CREATE TABLE `Material` (
   `id_material` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
@@ -85,4 +85,61 @@ CREATE TABLE `Shape` (
   `id_shape` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `shape_type` MEDIUMTEXT NULL DEFAULT NULL,
   PRIMARY KEY (`id_shape`)
+);
+
+-- ---
+-- Table 'Dial'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Dial`;
+		
+CREATE TABLE `Dial` (
+  `id_dial` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `id_dial_type` INTEGER NULL DEFAULT NULL,
+  `id_indexes` INTEGER NULL DEFAULT NULL,
+  `id_material` INTEGER NULL DEFAULT NULL,
+  `id_hands` INTEGER NULL DEFAULT NULL,
+  `id_color` INTEGER NULL DEFAULT NULL,
+  `luminance` BINARY NULL DEFAULT NULL,
+  PRIMARY KEY (`id_dial`)
+);
+
+-- ---
+-- Table 'Dial_type'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Dial_type`;
+		
+CREATE TABLE `Dial_type` (
+  `id_dial_type` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `dial_name` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_dial_type`)
+);
+
+-- ---
+-- Table 'Indexes'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Indexes`;
+		
+CREATE TABLE `Indexes` (
+  `id_indexes` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `indexes_type` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_indexes`)
+);
+
+-- ---
+-- Table 'Hands'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Hands`;
+		
+CREATE TABLE `Hands` (
+  `id_hands` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `hands_type` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_hands`)
 );
